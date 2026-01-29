5893d754b05a69575d3232cf5931bca7074fb06f
carlosdknavarro-sudo <carlosdknavarro@gmail.com>
2026-01-29 17:26:06 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,7 @@
-  import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-export const supabase = createClient(
-  "https://loqvusparkrtyzkatxdt.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvcXZ1c3BhcmtydHl6a2F0eGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NDI1OTEsImV4cCI6MjA4NTExODU5MX0.I7Q31UmzJuWF_oh2dPBAdjTZDq7WGG8ZAV0VBfHz2M4"
-);
+const supabaseUrl = "https://loqvusparkrtyzkatxdt.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxvcXZ1c3BhcmtydHl6a2F0eGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NDI1OTEsImV4cCI6MjA4NTExODU5MX0.I7Q31UmzJuWF_oh2dPBAdjTZDq7WGG8ZAV0VBfHz2M4"
+;
 
-
-
+export const supabase = createClient(supabaseUrl, supabaseKey);
